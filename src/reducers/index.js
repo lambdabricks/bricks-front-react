2977897995c14f2b57c20e3fe38cf9cce70cfc3e
Daniel Garcia <pel_daniel@hotmail.com>
2015-11-21 03:01:43 -0600

    Move libraryReducer into its own file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,6 @@
 import { combineReducers } from 'redux'
-import {
-  INVALIDATE_LIBRARY,
-  REQUEST_LIBRARY,
-  RECEIVE_LIBRARY
-} from '../actions'
+import { library } from './libraryReducer'
 
-function library(state = { isFetching: false, items: {} }, action) {
-  switch (action.type) {
-    case RECEIVE_LIBRARY:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        items: action.items
-      })
-    case REQUEST_LIBRARY:
-      return Object.assign({}, state, {
-        isFetching: true,
-        didInvalidate: false,
-      })
-    default:
-      return state
-  }
-}
-
-const rootReducer = combineReducers({
+export default combineReducers({
   library
 })
-
-export default rootReducer
---
+++ src/reducers/libraryReducer.js
@@ -0,0 +1,23 @@
+import {
+  INVALIDATE_LIBRARY,
+  REQUEST_LIBRARY,
+  RECEIVE_LIBRARY
+} from '../actions'
+
+export function library(state = { isFetching: false, items: {} }, action) {
+  switch (action.type) {
+    case RECEIVE_LIBRARY:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        items: action.items
+      })
+    case REQUEST_LIBRARY:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false,
+      })
+    default:
+      return state
+  }
+}
