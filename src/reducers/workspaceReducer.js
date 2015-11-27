171f96f7abde0fb4342d7b921b8ab6db00301568
Daniel Garcia <pel_daniel@hotmail.com>
2015-11-27 04:49:42 -0600

    Refactor appendToInner in workspaceReducer

--- src/reducers/workspaceReducer.js
+++ src/reducers/workspaceReducer.js
@@ -31,51 +31,51 @@ const initialWorkspace = {
   }
 }
 
-export function workspace(state = initialWorkspace, action) {
+export const workspace = (state = initialWorkspace, action) => {
   switch (action.type) {
     case ADD_PRIMITIVE:
-      return Object.assign({}, state, {
-        ...state,
-        rootBrick: {
-          ...state.rootBrick,
-          inner: [
-            ...state.rootBrick.inner,
-            {
-              id: nextId(),
-              name: action.payload,
-              position: PrimitiveDefaults.defaultPosition,
-              type: Primitive,
-              // react or redux ignore pair with value `undefined`
-              value: null
-            }
-          ]
+      return appendToInner(
+        state,
+        {
+          id: nextId(),
+          name: action.payload,
+          position: PrimitiveDefaults.defaultPosition,
+          type: Primitive,
+          // react or redux ignore pair with value `undefined`
+          value: null
         }
-      })
+      )
     case ADD_BRICK:
-      return Object.assign({}, state, {
-        ...state,
-        rootBrick: {
-          ...state.rootBrick,
-          inner: [
-            ...state.rootBrick.inner,
-            {
-              id: nextId(),
-              inputSlots: [
-                { id: nextId() },
-                { id: nextId() }
-              ],
-              name: action.payload,
-              outputSlots: [
-                { id: nextId() }
-              ],
-              position: BrickDefaults.defaultPosition,
-              size: BrickDefaults.defaultSize,
-              type: Brick
-            }
-          ]
-        }
-      })
+      return appendToInner(
+        state,
+        {
+          id: nextId(),
+          inputSlots: [
+            { id: nextId() },
+            { id: nextId() }
+          ],
+          name: action.payload,
+          outputSlots: [
+            { id: nextId() }
+          ],
+          position: BrickDefaults.defaultPosition,
+          size: BrickDefaults.defaultSize,
+          type: Brick
+        })
     default:
       return state
   }
+}
+
+const appendToInner = (state, element) => {
+  return Object.assign({}, state, {
+    ...state,
+    rootBrick: {
+      ...state.rootBrick,
+      inner: [
+        ...state.rootBrick.inner,
+        element
+      ]
+    }
+  })
 }
