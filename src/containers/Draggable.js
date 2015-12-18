8a4749e7871d145fe750516aad7064e6c51af495
Daniel Garcia <pel_daniel@hotmail.com>
2015-12-18 04:38:23 -0600

    Only start drag on left click

--- src/containers/Draggable.js
+++ src/containers/Draggable.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux'
 import { startDrag } from '../actions'
 
+const LEFT = 0
+
 const mapDispatchToProps = (dispatch) => {
   return {
     handleMouseDown: (elementId, mouseEvent, elementPosition) => {
+      if(mouseEvent.button != LEFT)
+        return
+
       dispatch(
         startDrag(
           elementId,
