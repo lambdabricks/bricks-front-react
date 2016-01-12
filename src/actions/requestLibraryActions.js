e350551b6b3fe2743c47a12ef21afa12db7f7776
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-12 10:06:31 -0600

    Use ES6 arrow functions in requestLibraryActions

--- src/actions/requestLibraryActions.js
+++ src/actions/requestLibraryActions.js
@@ -4,26 +4,26 @@ export const REQUEST_LIBRARY = 'REQUEST_LIBRARY'
 export const RECEIVE_LIBRARY = 'RECEIVE_LIBRARY'
 export const INVALIDATE_LIBRARY = 'INVALIDATE_LIBRARY'
 
-export function invalidateLibrary() {
+export const invalidateLibrary = () => {
   return {
     type: INVALIDATE_LIBRARY,
   }
 }
 
-function requestLibrary() {
+const requestLibrary = () => {
   return {
     type: REQUEST_LIBRARY,
   }
 }
 
-function receiveLibrary(items) {
+const receiveLibrary = (items) => {
   return {
     type: RECEIVE_LIBRARY,
     items: items
   }
 }
 
-function fetchLibrary() {
+const fetchLibrary = () => {
   return dispatch => {
     dispatch(requestLibrary())
     return fetch(`http://demo5895613.mockable.io/library/js/1`)
@@ -32,7 +32,7 @@ function fetchLibrary() {
   }
 }
 
-function shouldFetchLibrary(state) {
+const shouldFetchLibrary = (state) => {
   // const posts = state.postsByReddit[reddit]
   // if (!posts) {
   //   return true
@@ -44,7 +44,7 @@ function shouldFetchLibrary(state) {
   return true
 }
 
-export function fetchLibraryIfNeeded() {
+export const fetchLibraryIfNeeded = () => {
   return (dispatch, getState) => {
     if (shouldFetchLibrary(getState())) {
       return dispatch(fetchLibrary())
