1c3091e3501597e14db16e0b72d4892388ee17be
Daniel Garcia <pel_daniel@hotmail.com>
2016-01-12 12:53:18 -0600

    RemoveSelectedElement in WorkspaceSurface mouseDown is screwing drag

--- src/containers/WorkspaceSurface.js
+++ src/containers/WorkspaceSurface.js
@@ -24,7 +24,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(moveElement(position))
     },
     removeSelectedElement: () => {
-      dispatch(removeSelectedElement())
+      // dispatch(removeSelectedElement())
     },
     selectElementOrStopDrag: (mousePosition) => {
       dispatch(selectElementOrStopDrag(mousePosition))
