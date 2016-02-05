a2ab95843d44053aedd4db7f939800ef9473d2d5
Daniel Garcia <pel_daniel@hotmail.com>
2016-02-04 20:56:56 -0600

    Eval all unit tests

--- src/reducers/workspace/evaluationReducer.js
+++ src/reducers/workspace/evaluationReducer.js
@@ -16,22 +16,25 @@ export const evaluateAllWorkspaces = (workspace, elementId) => {
 
   const outputSlotId = Object.keys(brick.outputSlots)[0]
   const outputSlot = brick.outputSlots[outputSlotId]
-  const brickOutput = evalBrick(brick, valueIds, workspace.unitTests[0])
 
-  const newUnitTest = Object.assign({}, workspace.unitTests[0], {
-    values: {
-      ...workspace.unitTests[0].values,
-      [outputSlotId]: {
-        componentName: BRICK,
-        type: typeof brickOutput,
-        value: brickOutput.toString()
+  const newUnitTests = workspace.unitTests.map((unitTest) => {
+    const brickOutput = evalBrick(brick, valueIds, unitTest)
+
+    return Object.assign({}, unitTest, {
+      values: {
+        ...unitTest.values,
+        [outputSlotId]: {
+          componentName: BRICK,
+          type: typeof brickOutput,
+          value: brickOutput.toString()
+        }
       }
-    }
+    })
   })
 
   return Object.assign({}, workspace, {
     ...workspace,
-    unitTests: [newUnitTest]
+    unitTests: newUnitTests
   })
 }
 
