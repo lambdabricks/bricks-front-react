6a7f26c4c523bd78d3df69aed0a67f1f4edc4594
Daniel Garcia <pel_daniel@hotmail.com>
2016-03-15 04:23:26 -0600

    Only show test result when test output has value & type

--- src/utils/unitTestUtils.js
+++ src/utils/unitTestUtils.js
@@ -4,9 +4,11 @@ export const getFillColor = (mainBrick, unitTest) => {
   const testOutputId = mainBrick.testOutputIds[0]
   const { valueId } = mainBrick.outputSlots[testOutputId]
   const value = unitTest.values[valueId]
+  const testOutputValue = unitTest.values[testOutputId]
 
-  if(value && value.type && value.value) {
-    const testOutputValue = unitTest.values[testOutputId]
+  if(value && value.type && value.value &&
+     testOutputValue && testOutputValue.type && testOutputValue.value
+  ) {
     return testColors[
       value.type == testOutputValue.type &&
       value.value == testOutputValue.value
